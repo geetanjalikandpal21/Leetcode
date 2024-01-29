c8441780c6a5249e612e8d1c55678a4c26b3de76
Geetanjali Kandpal <79351146+geetanjalikandpal21@users.noreply.github.com>
2024-01-29 23:53:49 +0530

    Create Code.js

---
+++ Easy/Binary Tree Inorder Traversal/Code.js
@@ -0,0 +1,29 @@
+/** 
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+let inorderTraversal = function(root) {
+    let stack= [];
+    let result = [];
+    let current = root;
+    while(current !== null || stack.length > 0)
+    {
+while(current !== null)
+{
+    stack.push(current);
+current = current.left;
+}
+current = stack.pop();
+result.push(current.val);
+current = current.right;
+    }
+return result;
+};
